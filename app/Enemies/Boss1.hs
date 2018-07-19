d2b19d33758ec41e207c9c8397c2a32ce5285bb4
yasukatak83 <kazuhide.yasukata@g.softbank.co.jp>
2018-07-19 13:43:29 +0900

    Move Stage1 enemy as Boss1

--- app/Enemies/Boss1.hs
+++ app/Enemies/Boss1.hs
@@ -2,13 +2,22 @@ module Enemies.Boss1 where
 
 import qualified Bullet as B
 import qualified Enemy as E
+import Bullets.NWay
 import FRP.Yampa
+import FRP.Yampa.Vector2
+import FRP.Yampa.VectorSpace
 import Defs
 import Shared
+import Data.Function ((&))
 
-attack1 :: SF (E.Input, Vec2) (Event [B.Bullet], Enevt ())
-attack2 :: SF (E.Input, Vec2) (Event [B.Bullet], Enevt ())
-attack3 :: SF (E.Input, Vec2) (Event [B.Bullet], Enevt ())
-attack4 :: SF (E.Input, Vec2) (Event [B.Bullet], Enevt ())
-
-a = attack1 |> attack2 |> randomChoice [attack3, attack4]
+boss1 :: E.Enemy
+boss1 =
+  E.enemy 
+    ((vector2 0.0 80.0 & move zeroVector) >>> arr fst)
+    (wait_ 0.3 |>> recur_ 2 (attack1 |>> wait_ 0.3) >>> arr fst)
+    (arr snd >>> oArea)
+  where
+    oArea = outOfArea (-100.0) 100.0 100.0 (-100.0)
+    fast3way :: SF (E.Input, Vec2) (Event [B.Bullet], Event ())
+    fast3way = recur_ 4 (shotWait 0.3 (aimNWay 2 30.0 100.0)) |>> wait_ 0.2
+    attack1 = recur_ 3 (shotWait 0.5 $ aimedAllWay 10 50.0) |<>| fast3way
--- app/Stages/Stage1.hs
+++ app/Stages/Stage1.hs
@@ -1,26 +1,15 @@
 module Stages.Stage1 where
 
-import FRP.Yampa
-
 import qualified Stage as S
 import qualified Enemy as E
 import qualified Bullet as B
-import Enemies.AimShooter
+import Enemies.Boss1
 import Bullets.NWay
 import Shared
 import Data.Function ((&))
 import FRP.Yampa.Vector2
+import FRP.Yampa
 import Defs
 
 stage1 :: S.Stage
-stage1 = S.stage (Event [
-    E.enemy 
-      ((vector2 0.0 80.0 & move zeroVector) >>> arr fst)
---      ((repeatedly 0.3 () &&& arr (aimNWay 2 45.0 100.0)) >>> arr (uncurry tag))
-      (recur_ 2 fast3way |>> recur_ 3 (shotWait 0.5 $ aimedAllWay 10 50.0)>>> arr fst)
-      (arr snd >>> oArea)
-  ] --> constant NoEvent)
-  where
-    oArea = outOfArea (-100.0) 100.0 100.0 (-100.0)
-    fast3way :: SF (E.Input, Vec2) (Event [B.Bullet], Event ())
-    fast3way = recur_ 20 (shotWait 0.1 (aimNWay 2 30.0 100.0)) |>> wait_ 0.2
+stage1 = S.stage (after 0.2 [boss1])
